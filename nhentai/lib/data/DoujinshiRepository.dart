3891f67f1f718a90ba179aca8d261579c9fd64b9
Phuong Duy <duyphuong5126@gmail.com>
2021-12-18 21:29:47 +0700

    Disable backup. Increase download interval to 1 second

--- nhentai/lib/data/DoujinshiRepository.dart
+++ nhentai/lib/data/DoujinshiRepository.dart
@@ -156,7 +156,7 @@ class DoujinshiRepositoryImpl extends DoujinshiRepository {
           return pageUrlList;
         })
         .flatMap((pageUrlList) => Stream.fromIterable(pageUrlList))
-        .interval(Duration(milliseconds: 500))
+        .interval(Duration(seconds: 1))
         .flatMap((pageUrl) {
           print('DoujinshiRepository: try to download page $pageUrl');
           return _downloadPage(doujinshi.id, pageUrl);
